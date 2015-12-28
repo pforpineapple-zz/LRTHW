6aac2100a71ab8a5fc0225e89ec2109bcddd8306
Anas Arifi <anas.arifi@gmail.com>
2015-12-28 17:56:31 +0100

    Exercices ~ 35

---
+++ 30-39/ex34.rb
@@ -0,0 +1,10 @@
+animals = ['bear', 'ruby', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+puts "1. The animal at one is #{animals[1]}"
+puts "2. The third animal is #{animals[2]}"
+puts "3. The first animal is #{animals[0]}"
+puts "4. The animal at 3 is #{animals[3]}"
+puts "5. The fifth animal is #{animals[6]}"
+puts "6. The animal at 2 is #{animals[2]}"
+puts "7. The sixth animal is #{animals[7]}"
+puts "8. The animal at 4 is #{animals[4]}"
