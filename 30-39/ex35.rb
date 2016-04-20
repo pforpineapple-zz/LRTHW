74bcb9555076d6805c8d617930dd8bccf284a19d
Anas Arifi <anas.arifi@gmail.com>
2016-04-20 20:55:01 +0200

    ex35

--- 30-39/ex35.rb
+++ 30-39/ex35.rb
@@ -2,10 +2,10 @@ def gold_room
 	puts "This room is full of gold. How much do you take?"
 
 	print ">"
-	choce = $stdin.gets.chomp
+	choice = $stdin.gets.chomp
 
 	# this line has a bug, so fix it / EDIT : fixed (spaces before argument)
-	if choice.include?("0") || choice.include?("1")
+	if choice =~ /\d/
 		how_much = choice.to_i
 	else
 		dead("Man, learn to type a number.")
